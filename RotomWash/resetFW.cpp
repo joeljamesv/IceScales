fa7354d60bb0128939cccc7dfa508ec6284cbf8f
Joel James <joejamesv7@gmail.com>
2026-06-11 14:38:46 +0000

    add resetFW binary

--- RotomWash/resetFW.cpp
+++ RotomWash/resetFW.cpp
@@ -1,12 +1,15 @@
+#include "../Dex/logger.hpp"
+
 #include <filesystem>
+#include <format>
 #include <fstream>
 #include <iostream>
 
 int main() {
+  Dex logger;
   std::filesystem::path sysPath = "../../../Firmware";
   std::filesystem::remove_all(sysPath);
 
-  if (std::filesystem::exists(sysPath)) {
-    std::cout << "Not able to remove\n";
-  }
+  logger.logJournal(
+      "Rotom", std::format("Unable to remove {}", sysPath.string()), "ERROR");
 }
